bd92da3f4cba8c910c7aa1e44f1fca4e9883d1d8
bharti2022 <bharti.sagar@zemosolabs.com>
2022-08-12 11:25:49 +0530

    Assignment 6 google chat

---
+++ Assigment6_google_chat/send_message.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+from json import dumps
+from httplib2 import Http
+
+def main():
+    """Send message via Hangouts Chat incoming webhook"""
+    url = 'https://chat.googleapis.com/v1/spaces/AAAA_HerXTU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=FpdINKhgWAAzYCqsBR2YO_Cs1U8LcQmETxDc95-4rL0%3D'
+    message = {
+        'text' : 'git pull success !!!!'}
+
+    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+
+    http_obj = Http()
+
+    response = http_obj.request(
+        uri=url,
+        method='POST',
+        headers=message_headers,
+        body=dumps(message),
+    )
+
+    print(response)
+
+if __name__ == '__main__':
+    main()
